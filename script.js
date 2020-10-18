a4b6195c617dbbc01e6177feb58a8e5eb1c2eccc
Maksym <maksympata@maksyms-air.home>
2020-10-18 20:31:27 +0200

    Reorganising code and adding comments

--- script.js
+++ script.js
@@ -2,11 +2,27 @@ const button = document.querySelector('button');
 const input = document.querySelector('input');
 const ul = document.querySelector('ul');
 
-button.addEventListener('click', () => {
-    console.log("click");
-    const li = document.createElement('li');
-    if(input.value !== 0){
-        li.appendChild(document.createTextNode(input.value));
-        ul.appendChild(li);
+const createListElement = () =>{
+    const li = document.createElement('li'); // create list item
+    li.appendChild(document.createTextNode(input.value)); //create text node with text from input and attaches it to li
+    ul.appendChild(li); // atach list element with text node to the unordered list
+    input.value = ""; // clear input field
+}
+const isEnterClicked = (kC) =>{
+    return kC === 13;
+}
+
+const addListElementAfterClick = () =>{
+    if(input.value !== ""){ // check if input is empty
+        createListElement();
     }
-})
+}
+const addListElementAfterKeypress = (keyClicked) =>{
+    if(isEnterClicked(keyClicked.keyCode) && input.value !== ""){
+        createListElement();
+    }
+}
+
+button.addEventListener('click', addListElementAfterClick);
+
+input.addEventListener('keypress', addListElementAfterKeypress);
