3f05678aeb78b43380268eb8e87ae1b72a95c498
Maksym <maksympata@maksyms-air.home>
2020-10-18 18:10:48 +0200

    Fixed the unordered list items to be on the center of the page

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const ul = document.querySelector('ul');
 button.addEventListener('click', () => {
     console.log("click");
     const li = document.createElement('li');
-    if(input.value !== ""){
+    if(input.value !== 0){
         li.appendChild(document.createTextNode(input.value));
         ul.appendChild(li);
     }
