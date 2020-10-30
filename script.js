71b4626319de32961b8f632eb56eef4745a00394
Maksym <maksympata@Maksyms-MacBook-Air.local>
2020-10-30 18:52:49 +0100

    Add button to delete list items

--- script.js
+++ script.js
@@ -4,11 +4,18 @@ const ul = document.querySelector('ul');
 
 const createListElement = () =>{
     const li = document.createElement('li'); // create list item
+    const deleteButton = document.createElement('button'); // create button
     li.appendChild(document.createTextNode(input.value)); //create text node with text from input and attaches it to li
+    deleteButton.appendChild(document.createTextNode('X')); // create X in button
+    li.appendChild(deleteButton); // attach button to list element
     ul.appendChild(li); // atach list element with text node to the unordered list
     input.value = ""; // clear input field
     li.addEventListener('click', () =>{
         li.classList.toggle('done');
+    })
+    // if button 
+    deleteButton.addEventListener('click', () =>{
+        ul.removeChild(li);
     })
 }
 const isEnterClicked = (kC) =>{
